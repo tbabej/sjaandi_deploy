6573056dace7b0f904043b9b3640cb7327bf4685
pechyonkin <maxim.pechyonkin@gmail.com>
2019-08-17 15:08:05 -0400

    Receive, save and load uploaded images.
    Images are saved to upload folder, and then handled by imageio
    to load them into numpy format. In addition, the upload folder is
    cleared every time the '/' endpoint is requested from the browser.

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 import os
 from pathlib import Path
+import shutil
 
 from flask import Flask
 from flask import render_template
 from flask import request
 from flask_dropzone import Dropzone
+import numpy as np
+import imageio
 
 
-basedir = Path(__file__).resolve()  # absolute path of the app.py
+BASEDIR: Path = Path(__file__).resolve().parents[0]
+UPLOADS_DIR: Path = BASEDIR/'uploads'
+debug = True
+
 
 app = Flask(__name__)
 
 app.config.update(
-    UPLOADED_PATH=os.path.join(basedir, 'uploads'),
+    UPLOADED_PATH=os.path.join(BASEDIR, 'uploads'),
     # Flask-Dropzone config:
     DROPZONE_ALLOWED_FILE_TYPE='image',
     DROPZONE_MAX_FILE_SIZE=3,
@@ -24,19 +30,38 @@ app.config.update(
 dropzone = Dropzone(app)
 
 
+def clear_uploads_folder(folder: Path = UPLOADS_DIR):
+    shutil.rmtree(folder)
+    os.makedirs(UPLOADS_DIR)
+
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    if debug:
+        clear_uploads_folder()
+        # TODO: set up logging instead of print statements
+        print("CLEARED UPLOAD FOLDER")
     if request.method == 'POST':
+        # f: werkzeug.datastructures.FileStorage
+        # TODO: handle cases with different images with the same name
         for key, f in request.files.items():
             if key.startswith('file'):
-                print('RECEIVED AN IMAGE!')
-                # f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
+                # img = imageio.imread(f)
+                # print(f"IMAGE SHAPE: {img.shape}")
+                f.save(str(UPLOADS_DIR/f.filename))
+                print('RECEIVED AND SAVED AN IMAGE!')
     return render_template('index.html')
 
 
 @app.route('/collage')
 def collage():
-    return 'BEHOLD! HEREIN LIES YOUR BEAUTIFUL COLLAGE!'
+    files = [fname for fname in UPLOADS_DIR.iterdir() if fname.is_file()]
+    for fname in files:
+        # Note: imageio can read image directly from file bytestream
+        img = imageio.imread(fname)[:,:,:3]  # remove alpha channel
+        print(img.shape)
+    return str(files)
+    # return 'BEHOLD! HEREIN LIES YOUR BEAUTIFUL COLLAGE!'
 
 
 if __name__ == "__main__":
