23e45c5a1d4dbc44cc8fde44814c0de8347e2262
pechyonkin <maxim.pechyonkin@gmail.com>
2019-08-22 10:12:14 -0400

    Finish collage functionality.
    Now the app does the core functions:
    - remembers the user (using sesssions)
    - uses Sjaandi and RasterFairy libraries to make collage
    - stores collages made by different users

--- app.py
+++ app.py
@@ -1,28 +1,39 @@
 import os
 from pathlib import Path
 import shutil
+import tempfile
+import uuid
 
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import session
+from flask_session import Session
 from flask_dropzone import Dropzone
-import numpy as np
 import imageio
+import numpy as np
+
+from src.usecases.mvp import *
 
 
 BASEDIR: Path = Path(__file__).resolve().parents[0]
-UPLOADS_DIR: Path = BASEDIR/'uploads'
+USERS_DIR: Path = BASEDIR/'static'/'users'
 debug = True
 
 
 app = Flask(__name__)
+# Check Configuration section for more details
+SESSION_TYPE = 'redis'
+app.config.from_object(__name__)
+Session(app)
+
 
 app.config.update(
-    UPLOADED_PATH=os.path.join(BASEDIR, 'uploads'),
+    # UPLOADED_PATH=os.path.join(BASEDIR, 'uploads'), # possibly unnecessary
     # Flask-Dropzone config:
     DROPZONE_ALLOWED_FILE_TYPE='image',
     DROPZONE_MAX_FILE_SIZE=3,
-    DROPZONE_MAX_FILES=16,
+    DROPZONE_MAX_FILES=50,
     DROPZONE_UPLOAD_ON_CLICK=True,
     DROPZONE_REDIRECT_VIEW='collage',
 )
@@ -30,38 +41,101 @@ app.config.update(
 dropzone = Dropzone(app)
 
 
-def clear_uploads_folder(folder: Path = UPLOADS_DIR):
-    shutil.rmtree(folder)
-    os.makedirs(UPLOADS_DIR)
+def make_dummy_collage(username: str, folder: str):
+    """
+    Make and save a dummy collage into user folder.
+
+    Uses a dummy image to pretend that is the collage.
+
+    :param username: unique user id
+    :param folder: stores images to make the collage
+    :return: None
+    """
+    DUMMY_IMAGE_PATH = 'dummy.png'
+    img = imageio.imread(DUMMY_IMAGE_PATH)[:,:,:3]
+    print(type(img))
+    save_folder: Path = USERS_DIR / username
+    if not save_folder.exists():
+        print("CREATING NEW USER'S DIRECTORY")
+        os.makedirs(save_folder)
+    filename = str(uuid.uuid4()) + '.png'
+    save_path = save_folder/filename
+    imageio.imwrite(save_path, img)
+
+
+def make_collage(user: str, data_path: str):
+    """
+    Make and save collage into user folder.
+
+    :param user: unique user id
+    :param data_path: stores images to make the collage
+    :return:
+    """
+    data = data_for_activations(data_path)
+    engine = VisualSearchEngine(data)
+    new_collage = engine.make_collage()
+    save_folder: Path = USERS_DIR/user
+    if not save_folder.exists():
+        print("CREATING NEW USER'S DIRECTORY")
+        os.makedirs(save_folder)
+    filename = str(uuid.uuid4()) + '.png'
+    save_path = save_folder/filename
+    imageio.imwrite(save_path, new_collage)
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    if debug:
-        clear_uploads_folder()
-        # TODO: set up logging instead of print statements
-        print("CLEARED UPLOAD FOLDER")
-    if request.method == 'POST':
+    """
+    1. check if user exists, if not, add it to session object
+    2. create a temporary folder to store uploaded images
+    3. save all uploaded images
+    4. if new user, make folder for collages, if old user, folder already exists
+    5. make collage
+    6. save collage to user folder
+    """
+    if request.method == 'GET':
+        pass
+    elif request.method == 'POST':
+        if 'user' not in session:
+            print('Creating new user.')
+            user_id = str(uuid.uuid4())
+            session['user'] = user_id
+        else:
+            print('Existing user.')
+            user_id = session['user']
         # TODO: handle cases with different images with the same name
         # TODO: handle when size is too small - can't upscale currently
         # f: werkzeug.datastructures.FileStorage
-        for key, f in request.files.items():
-            if key.startswith('file'):
-                # img = imageio.imread(f)
-                f.save(str(UPLOADS_DIR/f.filename))
-                print('RECEIVED AND SAVED AN IMAGE!')
+        with tempfile.TemporaryDirectory() as tempdir:
+            for key, f in request.files.items():
+                if key.startswith('file'):
+                    # img = imageio.imread(f)
+                    f.save(os.path.join(tempdir, f.filename))
+                    print('RECEIVED AND SAVED AN IMAGE!')
+            print("ALL IMAGES UPLOADED.")
+            # make_dummy_collage(user_id, tempdir)
+            make_collage(user_id, tempdir)
+            print("COLLAGE MADE!")
     return render_template('index.html')
 
 
 @app.route('/collage')
 def collage():
-    files = [fname for fname in UPLOADS_DIR.iterdir() if fname.is_file()]
-    for fname in files:
-        # Note: imageio can read image directly from file bytestream
-        img = imageio.imread(fname)[:,:,:3]  # remove alpha channel
-        print(img.shape)
-    return str(files)
-    # return 'BEHOLD! HEREIN LIES YOUR BEAUTIFUL COLLAGE!'
+    if 'user' in session:
+        user_path = USERS_DIR/session['user']
+        collages = [str(f).split('sjaandi_deploy/')[1] for f in user_path.iterdir()]
+        print(collages)
+        return render_template('collages.html', collages=collages)
+    else:
+        return "<h1>You don't have any collages yet.</h1>"
+
+
+@app.route('/user')
+def user():
+    if 'user' in session:
+        return f"Your user id is: {session['user']}"
+    else:
+        return "This is a new user."
 
 
 if __name__ == "__main__":
